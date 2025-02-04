f25ee91a0835b1979f4f01cc008f1e712322f7f3
Saw Theme <127903690+SwthSawTheme@users.noreply.github.com>
2025-02-04 10:27:45 -0300

    Código simples pra verificar se um número é par ou impar

---
+++ Cpp/ParOuImpar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int num = 3;
+    
+    if (num % 2 == 0){
+        std::cout << "Par" << std::endl;
+    } else{
+        std::cout << "Impar" << std::endl;
+    }
+    return 0;
+}
